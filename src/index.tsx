3dc966eb491866bdf2cf0be96556dbcdf5f0d695
enthusiast94 <manas.bajaj94@gmail.com>
2019-12-15 13:53:53 +0000

    Tutorial completed

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { App, Didact } from './didact';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = document.getElementById('root');
+if (!root) throw new Error('root not found in DOM');
+
+Didact.render(App, root);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
